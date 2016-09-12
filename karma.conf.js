9b14d7642b4d6aefdbda09290f3c2d168b286db0
Daniel Koza <dakolech@gmail.com>
2016-09-12 11:50:17 +0200

    update test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,1 @@
-module.exports = require('./ng2-config/karma.conf.js')('./spec-bundle.js', require('./ng2-config/webpack.test'), '../src');
+module.exports = require('./ng2-config/karma.conf.js')('./spec-bundle.js', require('./ng2-config/webpack.test'), './http-wrapper/src');
